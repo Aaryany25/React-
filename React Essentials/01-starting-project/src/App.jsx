865ed9ea9282b0e8417d42ec7fe6d0e9858e5133
Aaryan <aaryany.2505@gmail.com>
2024-11-22 20:35:29 +0530

    created Component Folder and add Header Function in it

--- React Essentials/01-starting-project/src/App.jsx
+++ React Essentials/01-starting-project/src/App.jsx
@@ -1,33 +1,6 @@
-import reactImg from './assets/react-core-concepts.png'
 import {CORE_CONCEPTS} from './data.js'
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core','Important'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const Description = reactDescriptions[genRandomInt(3)]
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {Description} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreComponent(props){
-  return(
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  )
-}
+import Header from './Components/Header.jsx';
+import CoreComponent from './Components/CoreConcepts.jsx';
 function App() {
   return (
     <div>
